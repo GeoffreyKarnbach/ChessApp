7c64697fe3d8c15c0e11fb321b12bcb6131a4b95
DerGoyote <geoffrey.karnbach@gmail.com>
2020-11-02 19:51:41 +0100

    Third Draft

--- chess.py
+++ chess.py
@@ -3,41 +3,58 @@ from tkinter import*
 lastRect=0
 lastCoords=[]
 modifiable=True
+imagesRefs=[]
+possible=[]
 
 board=[["*" for lopp in range(8)] for loop in range(8)]
-board[0]=["R1","k1","B1","Q1","K1","B1","k1","R1"]
-board[1]=["P1" for loop in range(8)]
-board[6]=["P2" for loop in range(8)]
-board[7]=["R2","k2","B2","Q2","K2","B2","k2","R2"]
+board[0]=["r","n","b","q","k","b","n","r"]
+board[1]=["p" for loop in range(8)]
+board[6]=["P" for loop in range(8)]
+board[7]=["R","N","B","Q","K","B","N","R"]
 
-figures={"P1":"pawn1.png","k1":"knight1.png","B1":"bishop1.png","R1":"rooks1.png","Q1":"queen1.png","K1":"king1.png",\
-        "P2":"pawn2.png","k2":"knight2.png","B2":"bishop2.png","R2":"rooks2.png","Q2":"queen2.png","K2":"king2.png"}
+figures={"p":"pawn1.png","n":"knight1.png","b":"bishop1.png","r":"rooks1.png","q":"queen1.png","k":"king1.png",\
+        "P":"pawn2.png","N":"knight2.png","B":"bishop2.png","R":"rooks2.png","Q":"queen2.png","K":"king2.png"}
 
-#figures["P1"] -> pawn1.png
-
-'''
-P = pawns
-k = knights
-B = bishops
-R = rooks
-Q = queen
-K = king
-'''
+possibleMoves={"P":[(-1,0),(-2,0)],\
+               "p":[(1,0),(2,0)],\
+               "n":[(1,2),(-1,2),(1,-2),(-1,-2),(2,1),(-2,1),(2,-1),(-2,-1),],\
+               "N":[(1,2),(-1,2),(1,-2),(-1,-2),(2,1),(-2,1),(2,-1),(-2,-1),],\
+               "b":[(-8,-8),(-7,-7),(-6,-6),(-5,-5),(-4,-4),(-3,-3),(-2,-2),(-1,-1),(+1,+1),(+2,+2),(+3,+3),(+4,+4),(+5,+5),(+6,+6),(+7,+7),(+8,+8),\
+                    (+8,-8),(+7,-7),(+6,-6),(+5,-5),(+4,-4),(+3,-3),(+2,-2),(+1,-1),(-1,+1),(-2,+2),(-3,+3),(-4,+4),(-5,+5),(-6,+6),(-7,+7),(-8,+8)],
+               "B":[(-8,-8),(-7,-7),(-6,-6),(-5,-5),(-4,-4),(-3,-3),(-2,-2),(-1,-1),(+1,+1),(+2,+2),(+3,+3),(+4,+4),(+5,+5),(+6,+6),(+7,+7),(+8,+8),\
+                    (+8,-8),(+7,-7),(+6,-6),(+5,-5),(+4,-4),(+3,-3),(+2,-2),(+1,-1),(-1,+1),(-2,+2),(-3,+3),(-4,+4),(-5,+5),(-6,+6),(-7,+7),(-8,+8)]}
 
 def array_to_fen(array):
     pass
-    #"rnbnkqrb/pppppppp/8/8/8/8/PPPPPPPP/RNBNKQRB w KQkq - 0 1"
 
-def get_moves(position):
+def fen_to_array():
     pass
 
+def get_moves(position):
+    global board
+    moves=[]
+    if board[position[1]][position[0]] != "*":
+        liste=possibleMoves[board[position[1]][position[0]]]
+        for loop in liste:
+            moves.append((loop[0]+position[1],loop[1]+position[0]))
+    return moves
+
+def show_moves(moves):
+    global possible
+    for loop in moves:
+        possible.append(can.create_rectangle((loop[1])*100+2,(loop[0])*100+2,(loop[1])*100+98,(loop[0])*100+98,outline="green",width=4))
+        print(loop)
+
 def confirm_case(event):
-    global lastCoords,modifiable
+    global lastCoords,modifiable,possible
     if lastCoords!=[]:
         if modifiable:
             modifiable=False
+            show_moves(get_moves(((lastCoords[0]-2)//100,(lastCoords[1]-2)//100)))
         else:
             modifiable=True
+            for loop in possible:
+                can.delete(loop)
     else:
         print("Select a case before confirming")
 
@@ -52,19 +69,22 @@ def click(event):
             can.delete(lastRect)
             lastRect=can.create_rectangle((event.x//100)*100+2,(event.y//100)*100+2,(event.x//100)*100+98,(event.y//100)*100+98,outline="red",width=4)
             lastCoords=[(event.x//100)*100+2,(event.y//100)*100+2,(event.x//100)*100+98,(event.y//100)*100+98]
-    else:
-        print((lastCoords[0]-2)//100,(lastCoords[1]-2)//100)
 
 
 def update_UI(board):
+    global imagesRefs
     for loop in range(8):
         for lopp in range(8):
-            print(board[loop][lopp],end=" ")
-        print()
+            try:
+                fileName="Images/"+figures[board[loop][lopp]]
+                imagesRefs.append(PhotoImage(file=fileName))
+                can.create_image(lopp*100+20,loop*100+20,image=imagesRefs[-1],anchor=NW)
+            except:
+                pass
 
 
 def draw_board():
-    colors=["black","white"]
+    colors=["#f2d08a","white"]
     offset,current=0,0
     for loop in range(8):
         offset=(offset+1)%2
@@ -76,9 +96,8 @@ def draw_board():
 fen=Tk()
 fen.title("Chess")
 fen.geometry("850x850")
-fen.config(bg="grey")
 
-can=Canvas(fen,width=797,height=797,bg="grey")
+can=Canvas(fen,width=797,height=797,bg="grey",borderwidth=3)
 can.grid(column=0,row=0,padx=25,pady=25)
 
 draw_board()
